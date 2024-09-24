726c7b37cabd7f358b7ac2af4effd5b98d504548
Akshay Narayanan B <90465549+NarayanAkshay11@users.noreply.github.com>
2024-09-24 15:39:16 +0800

    Update main.c

--- Y2/SC2104/Lab2/main.c
+++ Y2/SC2104/Lab2/main.c
@@ -1,27 +1,29 @@
 /* USER CODE BEGIN Header */
 /**
- ******************************************************************************
- * @file           : main.c
- * @brief          : Main program body
- ******************************************************************************
- * @attention
- *
- * Copyright (c) 2022 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
- */
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  * Code Written By : Narak11
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "MPU6050.h"
+#include <math.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "ICM20948.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -39,19 +41,18 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-I2C_HandleTypeDef hi2c1;
+ I2C_HandleTypeDef hi2c2;
 
 UART_HandleTypeDef huart3;
 
 /* USER CODE BEGIN PV */
-ICM20948 imu; // declare a variable with the structure type ICM20948
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_I2C1_Init(void);
+static void MX_I2C2_Init(void);
 static void MX_USART3_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
@@ -59,212 +60,257 @@ static void MX_USART3_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+uint8_t IMU_ADDR = 0X68 << 1;
+
+IMU_Data imu;
+float pitch_acc;
+float roll_acc;
+char sbuf[16][15]; //16 rows just in case you wanna keep 2 graphs together
+float pitch_gyro = 0;
+float roll_gyro = 0 ;
+float yaw_gyro = 0;
+float pitch_fusion_comp = 0;
+float roll_fusion_comp = 0;
+float Var_gyro = 16;
+float Var_acc = 9;
+float roll_var_g = 4;
+float pitch_var_g = 4;
+float KG_roll;
+float KG_pitch;
+float roll_KF = 0;
+float pitch_KF = 0;
+float dt;
+uint32_t millisOld, millisNow;
 
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
-int main(void) {
-	/* USER CODE BEGIN 1 */
-  	HAL_NVIC_SystemReset();
-	/* USER CODE END 1 */
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+  //HAL_NVIC_SystemReset();
+  /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-	/* USER CODE END Init */
+  /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-	/* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_I2C1_Init();
-	MX_USART3_UART_Init();
-	/* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C2_Init();
+  MX_USART3_UART_Init();
+  /* USER CODE BEGIN 2 */
+  uint8_t status = IMU_Initialise(&imu, &hi2c2, &huart3);
+  /* USER CODE END 2 */
 
-	/* USER CODE END 2 */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+  IMU_AccelRead(&imu);
+  IMU_GyroRead(&imu);
 
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
+  /* The below block is done just to check the values of accelerometer, only uncomment and do it in case the TA asks, else submit the final code and enjoy */
+  //sprintf(&sbuf, "%5.2f , %5.2f , %5.2f  \n\r", imu.acc[0], imu.acc[1], imu.acc[2]);
+  //sprintf(&sbuf[0], "%5.2f , ", imu.acc[0]);
+  //sprintf(&sbuf[1], "%5.2f , ", imu.acc[1]);
+  //sprintf(&sbuf[2], "%5.2f , ", imu.acc[2]);
+  //HAL_UART_Transmit(&huart3, sbuf, 60, HAL_MAX_DELAY);
+  //HAL_UART_Transmit(&huart3, "\n\r", 2, HAL_MAX_DELAY);
+  //HAL_Delay(100);
 
-	uint8_t status = IMU_Initialise(&imu, &hi2c1, &huart3);
-	while (1) {
-		uint8_t sbuf[6][10] = { 0 };
-		//uint8_t sbuf[60] = { 0 };
-		/* USER CODE END WHILE */
-		IMU_AccelRead(&imu);
-		IMU_GyroRead(&imu);
+  pitch_acc = atan2(imu.acc[0], imu.acc[2]);
+  pitch_acc = -pitch_acc * 57.3;
 
-		//sprintf(&sbuf[0], "%5.2f ", imu.acc[0]);
-		//sprintf(&sbuf[10], "%5.2f ", imu.acc[1]);
-		//sprintf(&sbuf[20], "%5.2f ", imu.acc[2]);
-		//sprintf(&sbuf[30], "%5.2f ", imu.gyro[0]);
-		//sprintf(&sbuf[40], "%5.2f ", imu.gyro[1]);
-		//sprintf(&sbuf[50], "%5.2f ", imu.gyro[2]);
+  roll_acc = atan2(imu.acc[1], imu.acc[2]);
+  roll_acc = -roll_acc * 57.3;
 
-		//HAL_UART_Transmit(&huart3, sbuf, 60, HAL_MAX_DELAY);
-		//HAL_UART_Transmit(&huart3, "\n\r", 2, HAL_MAX_DELAY);
-		//HAL_Delay(100);
+  millisNow = HAL_GetTick();
+  dt = (millisNow - millisOld) * 0.001;
+  millisOld = millisNow;
 
-		//for (int i = 0; i < 3; i++) {
-		//	sprintf(sbuf[i], "%5.2f , ", imu.acc[i]);
-		//	HAL_UART_Transmit(&huart3, sbuf[i], 10, HAL_MAX_DELAY);
-		//}
-		//HAL_Delay(100);
-		//for (int i = 3; i < 6; i++) {
-		//	sprintf(sbuf[i], "%5.2f , ", imu.gyro[i-3]);
-		//	HAL_UART_Transmit(&huart3, sbuf[i], 10, HAL_MAX_DELAY);
-		//}
-		sprintf(&sbuf[0], "%5.2f , ", imu.acc[0]);
-		sprintf(&sbuf[1], "%5.2f , ", imu.acc[1]);
-		sprintf(&sbuf[2], "%5.2f , ", imu.acc[2]);
-		HAL_UART_Transmit(&huart3, sbuf, 60, HAL_MAX_DELAY);
-		HAL_UART_Transmit(&huart3, "\n\r", 2, HAL_MAX_DELAY);
-		HAL_Delay(100);
-		//sprintf(sbuf[2], "\n\r");
-		//HAL_URAT_Transmit(&huart3, sbuf[2], 2, HAL_MAX_DELAY);
 
-		//for (int i = 3; i < 6; i++) {
-		//	sprintf(sbuf[i], "%5.2f ", imu.gyro[i]);
-		//	HAL_UART_Transmit(&huart3, sbuf[i], 10, HAL_MAX_DELAY);
-		//}
-		//HAL_URAT_Transmit(&huart3, "\n\r", 2, HAL_MAX_DELAY);
+  pitch_gyro += imu.gyro[1] * dt ; // Integrate the angular velocity of x * dt
+  roll_gyro += imu.gyro[0] * dt ; //Integrate the angular velocity of y  * dt
+  yaw_gyro += imu.gyro[2] * dt ; //Yaw only if you wanna overdo n be a K155-455
 
-		/* USER CODE BEGIN 3 */
-	}
-	/* USER CODE END 3 */
+  /* Competency Filter Implementation */
+  pitch_fusion_comp = 0.05*pitch_acc + 0.95*(pitch_fusion_comp + (imu.gyro[1])*dt);
+  roll_fusion_comp = 0.05*roll_acc + 0.95*(roll_fusion_comp + (imu.gyro[1])*dt);
+
+  /* Kalman Filter Implementation */
+  roll_KF = roll_KF + imu.gyro[0]*dt;
+  pitch_KF = pitch_KF + imu.gyro[1]*dt;
+
+  roll_var_g = roll_var_g + dt*dt*Var_gyro;
+  pitch_var_g = pitch_var_g + dt*dt*Var_gyro;
+
+  KG_roll = roll_var_g/(roll_var_g + Var_acc);
+  KG_pitch = pitch_var_g/(pitch_var_g + Var_acc);
+
+  roll_KF = roll_KF + KG_roll*(roll_acc - roll_KF);
+  pitch_KF = pitch_KF + KG_pitch*(pitch_acc - pitch_KF);
+
+  roll_var_g = (1- KG_roll)*roll_var_g;
+  pitch_var_g = (1- KG_pitch)*pitch_var_g;
+
+
+  /* Final Printing of Values for SerialPort */
+  sprintf(sbuf ,"%5.2f, %5.2f, %5.2f, %5.2f, %5.2f, %5.2f, %5.2f, %5.2f ", pitch_acc, roll_acc, pitch_fusion_comp, roll_fusion_comp, roll_KF, pitch_KF, pitch_gyro, roll_gyro );
+  HAL_UART_Transmit(&huart3, sbuf, 60, HAL_MAX_DELAY);
+  HAL_UART_Transmit(&huart3, "\n\r", 2, HAL_MAX_DELAY); //New Line
+  HAL_Delay(100);
+
+
+
+
+
+
+
+
+
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
-void SystemClock_Config(void) {
-	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-	/** Configure the main internal regulator output voltage
-	 */
-	__HAL_RCC_PWR_CLK_ENABLE();
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-		Error_Handler();
-	}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
-		Error_Handler();
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /**
- * @brief I2C1 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_I2C1_Init(void) {
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
 
-	/* USER CODE BEGIN I2C1_Init 0 */
+  /* USER CODE BEGIN I2C2_Init 0 */
 
-	/* USER CODE END I2C1_Init 0 */
+  /* USER CODE END I2C2_Init 0 */
 
-	/* USER CODE BEGIN I2C1_Init 1 */
+  /* USER CODE BEGIN I2C2_Init 1 */
 
-	/* USER CODE END I2C1_Init 1 */
-	hi2c1.Instance = I2C1;
-	hi2c1.Init.ClockSpeed = 100000;
-	hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-	hi2c1.Init.OwnAddress1 = 0;
-	hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-	hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-	hi2c1.Init.OwnAddress2 = 0;
-	hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-	hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-	if (HAL_I2C_Init(&hi2c1) != HAL_OK) {
-		Error_Handler();
-	}
-	/* USER CODE BEGIN I2C1_Init 2 */
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.ClockSpeed = 100000;
+  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
 
-	/* USER CODE END I2C1_Init 2 */
-
-}
-
-/**
- * @brief USART3 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_USART3_UART_Init(void) {
-
-	/* USER CODE BEGIN USART3_Init 0 */
-
-	/* USER CODE END USART3_Init 0 */
-
-	/* USER CODE BEGIN USART3_Init 1 */
-
-	/* USER CODE END USART3_Init 1 */
-	huart3.Instance = USART3;
-	huart3.Init.BaudRate = 115200;
-	huart3.Init.WordLength = UART_WORDLENGTH_8B;
-	huart3.Init.StopBits = UART_STOPBITS_1;
-	huart3.Init.Parity = UART_PARITY_NONE;
-	huart3.Init.Mode = UART_MODE_TX_RX;
-	huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-	huart3.Init.OverSampling = UART_OVERSAMPLING_16;
-	if (HAL_UART_Init(&huart3) != HAL_OK) {
-		Error_Handler();
-	}
-	/* USER CODE BEGIN USART3_Init 2 */
-
-	/* USER CODE END USART3_Init 2 */
+  /* USER CODE END I2C2_Init 2 */
 
 }
 
 /**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
-static void MX_GPIO_Init(void) {
-	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+  * @brief USART3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART3_UART_Init(void)
+{
 
-	/* GPIO Ports Clock Enable */
-	__HAL_RCC_GPIOC_CLK_ENABLE();
-	__HAL_RCC_GPIOB_CLK_ENABLE();
-	__HAL_RCC_GPIOE_CLK_ENABLE();
+  /* USER CODE BEGIN USART3_Init 0 */
 
-	/*Configure GPIO pin : PE1 */
-	GPIO_InitStruct.Pin = GPIO_PIN_1;
-	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+  /* USER CODE END USART3_Init 0 */
+
+  /* USER CODE BEGIN USART3_Init 1 */
+
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 115200;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART3_Init 2 */
+
+  /* USER CODE END USART3_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
 }
 
@@ -273,17 +319,18 @@ static void MX_GPIO_Init(void) {
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
-void Error_Handler(void) {
-	/* USER CODE BEGIN Error_Handler_Debug */
-	/* User can add his own implementation to report the HAL error return state */
-	__disable_irq();
-	while (1) {
-
-	}
-	/* USER CODE END Error_Handler_Debug */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
@@ -297,7 +344,8 @@ void Error_Handler(void) {
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
+  /* User can add his own implementation to report the file name and
+line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
