4c73c590dcf57edcd2f6b9bb5fc13029e2c0ba89
Akshay Narayanan B <90465549+NarayanAkshay11@users.noreply.github.com>
2024-10-09 13:07:35 +0800

    Update P3.java

--- Y2/SC2002/Lab1/P3.java
+++ Y2/SC2002/Lab1/P3.java
@@ -1,41 +1,97 @@
-import java.util.Scanner;
-
-class P3 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter starting value: ");
-        int start = input.nextInt();
-        System.out.println("Enter ending value: ");
-        int end = input.nextInt();
-        System.out.println("Enter increment value: ");
-        int increment = input.nextInt();
-
-        if (start > end) {
-            System.out.println("Error input!!");
-        } else {
-            System.out.println("Using for loop:");
-            System.out.println("US$   S$");
-            for (int i = start; i <= end; i += increment) {
-                System.out.println(i + "   " + (i * 1.82));
-            }
-
-            System.out.println("Using while loop:");
-            System.out.println("US$   S$");
-            int i = start;
-            while (i <= end) {
-                System.out.println(i + "   " + (i * 1.82));
-                i += increment;
-            }
-
-            System.out.println("Using do/while loop:");
-            System.out.println("US$   S$");
-            i = start;
-            do {
-                System.out.println(i + "   " + (i * 1.82));
-                i += increment;
-            } while (i <= end);
-        }
-
-        input.close();
-    }
+package Lab1;
+import java.util.*;
+public class P3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int start = 0;
+		int end = 0;
+		int increment= 0;
+		int i;
+		String spaceNeeded = "            ";
+		while(true)
+		{
+			try
+			{
+				System.out.print("Input the starting US$: ");
+				start = sc.nextInt();
+			}
+			catch(InputMismatchException e)
+			{
+				System.out.println("Please input whole numbers only");
+				sc.nextLine();
+				continue;
+			}
+			break;
+		}
+		
+		while(true)
+		{
+			try
+			{
+				System.out.print("Input the ending US$: ");
+				end = sc.nextInt();
+			}
+			catch(InputMismatchException e)
+			{
+				System.out.println("Please input whole numbers only");
+				sc.nextLine();
+				continue;
+			}
+			if(end < start)
+			{
+				System.out.println("Error input!!");
+				continue;
+			}
+			
+			break;
+		}
+		while(true)
+		{
+			try
+			{
+				System.out.print("Input the increment: ");
+				increment = sc.nextInt();
+			}
+			catch(InputMismatchException e)
+			{
+				System.out.println("Please input whole numbers only");
+				sc.nextLine();
+				continue;
+			}
+			
+			sc.close();
+			
+			break;
+		}
+		System.out.println("US$         S$");
+		System.out.println("--------------");
+		
+		for(i = start; i <= end; i += increment)
+		{
+			System.out.printf("%d", i);
+			System.out.print(spaceNeeded.substring(0, spaceNeeded.length() - Integer.toString(i).length()));
+			System.out.printf(Float.toString(Math.round(i*1.82f*100)/100.0f) + "\n");
+		}
+		i = start;
+		System.out.println("\nUS$         S$");
+		System.out.println("--------------");
+		while(i <= end)
+		{
+			System.out.printf("%d", i);
+			System.out.print(spaceNeeded.substring(0, spaceNeeded.length() - Integer.toString(i).length()));
+			System.out.printf(Float.toString(Math.round(i*1.82f*100)/100.0f) + "\n");
+			i += increment;
+		}
+		i = start;
+		System.out.println("\nUS$         S$");
+		System.out.println("--------------");
+		do
+		{
+            System.out.printf("%d", i);
+			System.out.print(spaceNeeded.substring(0, spaceNeeded.length() - Integer.toString(i).length()));
+			System.out.printf(Float.toString(Math.round(i*1.82f*100)/100.0f) + "\n");
+			i += increment;
+		}
+        while(i <= end);
+	}
 }
