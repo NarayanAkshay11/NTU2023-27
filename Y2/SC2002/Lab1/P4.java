ce2dbc35d112a61c333a0732b1be2b301a5493ac
Akshay Narayanan B <90465549+NarayanAkshay11@users.noreply.github.com>
2024-10-09 13:09:16 +0800

    Update P4.java

--- Y2/SC2002/Lab1/P4.java
+++ Y2/SC2002/Lab1/P4.java
@@ -1,36 +1,55 @@
-import java.util.Scanner;
+package Lab1;
+import java.util.*;
+public class P4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int height = 0;
+		int i, j;
+		while(true)
+		{
+			try
+			{
+				System.out.print("Input the height: ");
+				height = sc.nextInt();
+			}
+			catch(InputMismatchException e)
+			{
+				System.out.println("Please input whole numbers only");
+				sc.nextLine();
+				continue;
+			}
+			if(height < 1)
+			{
+				System.out.println("Error input!!");
+				continue;
+			}
+					
+			break;
+		}
+		
+		sc.close();
+		for(i = 0; i < height; i++)
+		{
+			for(j = 0; j <= i; j++)
+			{
+				if(i % 2 == 0)
+				{
+					if(j % 2 == 0)
+						System.out.print("AA");
+					else
+						System.out.print("BB");
+				}
+				else
+				{
+					if(j % 2 == 0)
+						System.out.print("BB");
+					else
+						System.out.print("AA");
+				}
+			}
+			System.out.println("");
+		}
 
-class P4 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the height: ");
-        int height = input.nextInt();
+	}
 
-        if (height == 0) {
-            System.out.println("Error input!!");
-        } else {
-            for (int i = 1; i <= height; i++) {
-                if (i % 2 == 0) {
-                    for (int j = 0; j < i; j++) {
-                        if (j % 2 == 0) {
-                            System.out.print("B");
-                        } else {
-                            System.out.print("A");
-                        }
-                    }
-                } else {
-                    for (int j = 0; j < i; j++) {
-                        if (j % 2 == 0) {
-                            System.out.print("A");
-                        } else {
-                            System.out.print("B");
-                        }
-                    }
-                }
-                System.out.println();
-            }
-        }
-
-        input.close();
-    }
 }
