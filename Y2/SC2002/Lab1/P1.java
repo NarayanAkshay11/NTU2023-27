d2a566089c676d3d2f309063698c1d61843a4477
Akshay Narayanan B <90465549+NarayanAkshay11@users.noreply.github.com>
2024-10-09 13:04:45 +0800

    Update P1.java

--- Y2/SC2002/Lab1/P1.java
+++ Y2/SC2002/Lab1/P1.java
@@ -1,28 +1,34 @@
+package Lab1;
 import java.util.Scanner;
 
-class P1 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter your movie choice (A, C, D): ");
-        char choice = input.next().charAt(0);
+public class P1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("A. Action movie \nC. Comedy movie \nD. Drama movie");
+		System.out.print("Input your choice: ");
+		char choice = sc.next().charAt(0);
+		sc.close();
+		switch(choice)
+		{
+			case 'A':
+			case 'a':
+				System.out.printf("Action movie fan\n");
+				break;
+			
+			case 'C':
+			case 'c':
+				System.out.printf("Comedy movie fan\n");
+				break;
+			
+			case 'D':
+			case 'd':
+				System.out.printf("Drama movie fan\n");
+				break;
+			
+			default:
+				System.out.printf("Invalid choice\n");
+		}
 
-        switch (choice) {
-            case 'A':
-            case 'a':
-                System.out.println("Action movie fan");
-                break;
-            case 'C':
-            case 'c':
-                System.out.println("Comedy movie fan");
-                break;
-            case 'D':
-            case 'd':
-                System.out.println("Drama movie fan");
-                break;
-            default:
-                System.out.println("Invalid choice");
-        }
+	}
 
-        input.close();
-    }
 }
